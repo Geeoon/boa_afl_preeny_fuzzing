717457638b53bfe4c8f762635a4b57e1dcb07b77
Geeoon Chung <geeoon13@gmail.com>
2025-06-15 01:35:25 -0500

    Added note about libdesocket

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # starts the boa server using preeny to make STDIN the socket inputs
 # if binary is ran without preeny, it opens on localhost:3003
+# might use libdesocket instead of preeny
+# https://lolcads.github.io/posts/2022/02/libdesock/
+# PREENY_INFO=1 PREENY_ERROR=1 PREENY_DEBUG=1 LD_PRELOAD=./binaries/preeny/desock_dup.so ./binaries/boa-afl -c ./boa_root
 LD_PRELOAD=./binaries/preeny/desock.so ./binaries/boa-afl -c ./boa_root
