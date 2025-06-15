e4c40b071290c0507881b185731f93025e3a7215
Geeoon Chung <geeoon13@gmail.com>
2025-06-15 01:18:23 -0500

    Boa AFL

---
+++ binaries/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# starts the boa server using preeny to make STDIN the socket inputs
+# if binary is ran without preeny, it opens on localhost:3000
+LD_PRELOAD=preeny/desock.so ./boa-afl -c ../boa_root
