eb3cebd0405686ba541c968fbb15002726acbcaa
Geeoon Chung <geeoon13@gmail.com>
2025-06-15 01:00:55 -0500

    Added boa AFL build script

---
+++ src/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# this file build build boa with AFL++
+# the boa directory should contain the source code, i.e., the root of the boa project
+# at the end of this script, a boa-afl binary will be placed in the same directory
+cd boa
+./configure CC=afl-cc
+make
+cp ./src/boa ../boa-afl
