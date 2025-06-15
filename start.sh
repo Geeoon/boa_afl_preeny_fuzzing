cf9ccafc2d34354dbedbdd93d0021bcaa7d95d73
Geeoon Chung <geeoon13@gmail.com>
2025-06-15 11:37:02 -0500

    Fixed main and secondary fuzzers, but still complains about multiple main

--- start.sh
+++ start.sh
@@ -11,11 +11,19 @@ if [ $# -eq 0 ]
 then
     AFL_SKIP_CPUFREQ=1 AFL_PRELOAD=./binaries/preeny/desock.so afl-fuzz -i ./afl/seeds_dir -o ./afl/output_dir -- ./binaries/boa-afl -c ./boa_root
 else
-    AFL_SKIP_CPUFREQ=1 AFL_PRELOAD=./binaries/preeny/desock.so afl-fuzz -i ./afl/seeds_dir -o ./afl/sync_dir -M fuzzer01 -- ./binaries/boa-afl -c ./boa_root &
     instances=$(( $1 ))
-    for i in $(seq 1 $instances)
+
+    if [ $instances -lt 2 ]
+    then
+        echo Specified less than 2 instances. Please run the script without command line parameters.
+        exit 1
+    fi
+    
+    AFL_SKIP_CPUFREQ=1 AFL_PRELOAD=./binaries/preeny/desock.so afl-fuzz -i ./afl/seeds_dir -o ./afl/sync_dir -M fuzzer01 -- ./binaries/boa-afl -c ./boa_root &
+    for i in $(seq 2 $instances)
     do
-        name="fuzzer0$instances"
-        AFL_SKIP_CPUFREQ=1 AFL_PRELOAD=./binaries/preeny/desock.so afl-fuzz -i ./afl/seeds_dir -o ./afl/sync_dir -M $name -- ./binaries/boa-afl -c ./boa_root &
+        name="fuzzer0$i"
+        AFL_SKIP_CPUFREQ=1 AFL_PRELOAD=./binaries/preeny/desock.so afl-fuzz -i ./afl/seeds_dir -o ./afl/sync_dir -S $name -- ./binaries/boa-afl -c ./boa_root &
     done
+    # NOTE: run pkill boa to stop all fuzzers
 fi
