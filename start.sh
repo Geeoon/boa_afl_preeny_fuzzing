38baf0bd8c0095379b4a278dcee173eb5cb239de
Geeoon Chung <geeoon13@gmail.com>
2025-06-15 11:04:44 -0500

    AFL start script

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 # might use libdesocket instead of preeny
 # https://lolcads.github.io/posts/2022/02/libdesock/
 # PREENY_INFO=1 PREENY_ERROR=1 PREENY_DEBUG=1 LD_PRELOAD=./binaries/preeny/desock_dup.so ./binaries/boa-afl -c ./boa_root
-LD_PRELOAD=./binaries/preeny/desock.so ./binaries/boa-afl -c ./boa_root
+AFL_SKIP_CPUFREQ=1 PREENY_INFO=1 PREENY_ERROR=1 PREENY_DEBUG=1 AFL_PRELOAD=./binaries/preeny/desock.so afl-fuzz -i ./afl/seeds_dir -o ./afl/output_dir -- ./binaries/boa-afl -c ./boa_root
